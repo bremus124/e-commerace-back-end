05c4029532fd991b8df640fba8b55575ab80f3d1
Your Name <you@example.com>
2022-09-20 09:43:55 -0400

    tagroutes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,56 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:
+    [
+      {
+        model: Category,
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then(data=>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:
+    {
+      id: req.params.id
+    },
+    include:
+    [
+      {
+        model: Category,
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(data=>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 // create new product
